a6e9d379a18940fce53df963660a98dcc5d80fa7
Lord_Ralex <lordralex@gmail.com>
2015-11-13 22:57:24 -0600

    Update pom to support client/server building, add database

---
+++ database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS `notlet`;
+USE `notlet`;
+
+CREATE TABLE IF NOT EXISTS `auth` (
+  `username` varchar(16) NOT NULL,
+  `password` char(128) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `scores` (
+	`username` VARCHAR(16) NOT NULL,
+	`seed` INT(11) NOT NULL,
+	`score` INT(11) NOT NULL,
+	PRIMARY KEY (`username`, `seed`),
+	CONSTRAINT `FK_scores_auth` FOREIGN KEY (`username`) REFERENCES `auth` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
