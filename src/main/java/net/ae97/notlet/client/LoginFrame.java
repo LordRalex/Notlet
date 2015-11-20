84cf88863b62a1f38bc74fa5329032e7743f6db6
Lord_Ralex <lordralex@gmail.com>
2015-11-19 18:02:08 -0600

    Slightly clean up login frame

--- src/main/java/net/ae97/notlet/client/LoginFrame.java
+++ src/main/java/net/ae97/notlet/client/LoginFrame.java
@@ -1,54 +1,85 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 AE97
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package net.ae97.notlet.client;
 
-import javax.swing.*;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 
-@SuppressWarnings("serial")
 public class LoginFrame extends JFrame {
-	
-	public LoginFrame() {
-		initLogin();	
-	}
-	
-	private void initLogin() {
-		// game type -> enum
-		// seed
-		JButton loginButton = new JButton("Start!");
-		JPanel panel = new JPanel();
-		JTextField usernameField = new JTextField(15);
-		JPasswordField passwordField = new JPasswordField(15);
-		JLabel usernameLabel = new JLabel("Username");
-		JLabel passwordLabel = new JLabel("Password");
-		
-		JRadioButton continuous = new JRadioButton("continuous");
-		JRadioButton replay = new JRadioButton("replay");
-		ButtonGroup group = new ButtonGroup();
-		group.add(continuous);
-		group.add(replay);
-		
-		setTitle("Notlet Login");
-		setSize(300,175);
-		setLocation(500,280);
-		
-		panel.setLayout(null);
-		
-		usernameLabel.setBounds(10, 10, 80, 25);
-		passwordLabel.setBounds(10, 40, 80, 25);
-		usernameField.setBounds(100, 10, 160, 25);
-		passwordField.setBounds(100, 40, 160, 25);
-		loginButton.setBounds(10, 100, 80, 25);
-		continuous.setBounds(10,70,120,25);
-		replay.setBounds(192,70,80,25);
-		
-		panel.add(passwordLabel);
-		panel.add(usernameLabel);
-		panel.add(usernameField);
-		panel.add(passwordField);
-		panel.add(loginButton);
-		panel.add(continuous);
-		panel.add(replay);
-		
-		getContentPane().add(panel);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-	}
+
+    private final JButton loginButton;
+    private final JPanel panel;
+    private final JTextField usernameField;
+    private final JPasswordField passwordField;
+    private final JLabel usernameLabel, passwordLabel;
+    private final JRadioButton continuous, replay;
+    private final ButtonGroup group;
+
+    public LoginFrame() {
+        panel = new JPanel();
+        panel.setLayout(null);
+
+        loginButton = new JButton("Start!");
+        usernameField = new JTextField(15);
+        passwordField = new JPasswordField(15);
+        usernameLabel = new JLabel("Username");
+        passwordLabel = new JLabel("Password");
+        continuous = new JRadioButton("continuous");
+        replay = new JRadioButton("replay");
+        group = new ButtonGroup();
+
+        group.add(continuous);
+        group.add(replay);
+
+        setTitle("Notlet Login");
+        setSize(300, 175);
+        setLocation(500, 280);
+
+        usernameLabel.setBounds(10, 10, 80, 25);
+        passwordLabel.setBounds(10, 40, 80, 25);
+        usernameField.setBounds(100, 10, 160, 25);
+        passwordField.setBounds(100, 40, 160, 25);
+        loginButton.setBounds(10, 100, 80, 25);
+        continuous.setBounds(10, 70, 120, 25);
+        replay.setBounds(192, 70, 80, 25);
+
+        panel.add(passwordLabel);
+        panel.add(usernameLabel);
+        panel.add(usernameField);
+        panel.add(passwordField);
+        panel.add(loginButton);
+        panel.add(continuous);
+        panel.add(replay);
+
+        getContentPane().add(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
 }
