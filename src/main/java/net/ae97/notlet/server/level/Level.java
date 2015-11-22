4dd61287660c2b4c5116f428f7def468f05c4828
Lord_Ralex <lordralex@gmail.com>
2015-11-22 12:05:01 -0600

    Format and fix license headers

--- src/main/java/net/ae97/notlet/server/level/Level.java
+++ src/main/java/net/ae97/notlet/server/level/Level.java
@@ -1,40 +1,56 @@
-package net.ae97.notlet.server.level;
-
-/**
- * john
- * Level
+/*
+ * The MIT License
  *
- * Generates a boolean 2D array representing levels for the game engine to render
- * True means walkable space
- * False means a wall
+ * Copyright 2015 AE97
  *
- * Currently creates spindally spiderry passages.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
+package net.ae97.notlet.server.level;
 
 import java.util.Random;
 
+/**
+ * @author John
+ */
 public class Level {
 
-    private boolean[][] map;
-    private int seed;
-    private int size = 0;
+    private final int seed;
+    private boolean[][] map = new boolean[0][0];
 
     public Level() {
-
         this(new Random().nextInt());
-
     }
 
     public Level(int seed) {
-
         this.seed = seed;
-
     }
 
-    public void generate(){
-
-        Random RandBool = new Random(seed);
-        size = 100 + (RandBool.nextInt(50));
+    /**
+     * Generates a boolean 2D array representing levels for the game engine to render
+     * True means walkable space
+     * False means a wall
+     *
+     * Currently creates spindally spiderry passages.
+     */
+    public void generate() {
+        Random rng = new Random(seed);
+        int size = 100 + (rng.nextInt(50));
         map = new boolean[size][size];
 
         int i = 0;
@@ -43,11 +59,10 @@ public class Level {
         map[i][j] = true;
 
         while (i < size && j < size) {
-            if (RandBool.nextBoolean()) {
+            if (rng.nextBoolean()) {
                 map[i][j] = true;
                 i++;
-            }
-            else {
+            } else {
                 map[i][j] = true;
                 j++;
             }
@@ -59,70 +74,66 @@ public class Level {
                 map[i][j] = true;
                 j++;
             }
-        }
-        else if (i < j) {
+        } else if (i < j) {
             j--;
             while (i < size) {
                 map[i][j] = true;
                 i++;
             }
         }
-        fill(RandBool);
+        fill(rng);
     }
 
-    public void fill(Random RandBool) {
-
+    private void fill(Random rng) {
         int i = 0;
         int j = 0;
 
-        while (i < size && j < size) {
-            if (i > size && map[i+1][j]) {
+        while (i < map.length && j < map.length) {
+            if (i > map.length && map[i + 1][j]) {
                 i++;
-            }
-            else {
+            } else {
                 j++;
             }
-            if (RandBool.nextBoolean()) {
-                branch(RandBool, i, j);
+            if (rng.nextBoolean()) {
+                branch(rng, i, j);
             }
         }
     }
 
-    public void branch(Random RandBool, int i, int j) {
-
-        if (RandBool.nextBoolean()) {
-            while (i < size && j >= 0) {
-                if (RandBool.nextBoolean()) {
+    private void branch(Random rng, int i, int j) {
+        if (rng.nextBoolean()) {
+            while (i < map.length && j >= 0) {
+                if (rng.nextBoolean()) {
                     map[i][j] = true;
                     i++;
-                }
-                else {
+                } else {
                     map[i][j] = true;
                     j--;
                 }
             }
-        }
-        else {
-            while (i >= 0 && j < size) {
-                if (RandBool.nextBoolean()) {
+        } else {
+            while (i >= 0 && j < map.length) {
+                if (rng.nextBoolean()) {
                     map[i][j] = true;
                     i--;
-                }
-                else {
+                } else {
                     map[i][j] = true;
                     j++;
                 }
             }
         }
     }
+
     public boolean[][] getMap() {
         return map;
     }
+
     public int getSeed() {
         return seed;
     }
+
     public int getSize() {
-        return size;
+        return map.length;
     }
 
 }
